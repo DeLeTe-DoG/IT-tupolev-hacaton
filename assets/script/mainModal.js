620eee89642e211de897e767096bbd1d68f5927a
DeLeTe <delete_dog@mail.ru>
2025-03-29 23:56:02 +0300

    modal structure done

---
+++ assets/script/mainModal.js
@@ -0,0 +1,275 @@
+let timeTable_data = [
+    {
+        id: 1,
+        date: 'Понедельник',
+        classes: [
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Практика',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '9^40',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+        ],
+    },
+    {
+        id: 2,
+        date: 'Вторник',
+        classes: [
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '9:40',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 8,
+                auditory: 418,
+                time: '11:20',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Практика',
+                building: 8,
+                auditory: 418,
+                time: '11:20',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+        ],
+    },
+    {
+        id: 3,
+        date: 'Среда',
+        classes: [
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '9:40',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+        ],
+    },
+    {
+        id: 4,
+        date: 'Четверг',
+        classes: [
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Спецкурс',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '9:40',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+        ],
+    },
+    {
+        id: 5,
+        date: 'Пятница',
+        classes: [
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '9:40',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+        ],
+    },
+    {
+        id: 6,
+        date: 'Суббота',
+        classes: [
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '8:00',
+                week: 'чётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+            {
+                name: 'Пакеты прикладных проргамм в профессиональной деятельности',
+                type: 'Лабораторная работа',
+                building: 2,
+                auditory: 418,
+                time: '9:40',
+                week: 'нечётная',
+                teacher: 'Иванов Иван Иванович'
+            },
+        ],
+    },
+]
+
+let openModal = function() {
+    document.getElementById('app').insertAdjacentHTML('afterbegin', `
+        <div id="modal" class="modal-lists">
+            <div class="modal-list__wrapper">
+                
+            </div>
+        </div>
+    `)
+
+    timeTable_data.forEach(item => {
+        document.querySelector('.modal-list__wrapper').insertAdjacentHTML('beforeend', `
+            <div class="modal-list-item">
+                <div class="modal-list-item__header">
+                    <h2 class="time-table-date">${item.date}</h2>
+                    
+                </div>
+            </div>
+        `)
+    })
+    document.querySelector('.modal-list-item__header').insertAdjacentHTML('beforeend', `
+        <button class="close-modal" type="button">
+            <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="8.75903" y="29.9722" width="30" height="5" rx="2.5" transform="rotate(-45 8.75903 29.9722)" fill="#5269FF"/>
+                <rect x="12.4456" y="8.76758" width="30" height="5" rx="2.5" transform="rotate(45 12.4456 8.76758)" fill="#5269FF"/>
+            </svg>
+        </button>
+    `)
+
+    for (let i = 0; i <= timeTable_data.length; i++) {
+        timeTable_data[i]?.classes?.forEach(item => {
+            document.querySelectorAll('.modal-list-item')[i].insertAdjacentHTML('beforeend', `
+                <div class="modal-list-item__block">
+                    <div class="model-list-item-wrapper">
+                        <h3 class="day-name">${item.name}</h3>
+                        <div class="flex-wrapper">
+                            <p>${item.type}</p>
+                            <p>${item.building} здание - ${item.auditory} аудитория</p>
+                            <p>${item.time}</p>
+                            <p>${item.week} неделя</p>
+                        </div>
+                    </div>
+                    <div class="model-list-item-wrapper">
+                        <h3 class="modal-list-item__teacher">${item.teacher}</h3>
+                    </div>
+                </div>
+            `)
+        })
+    }
+}
+
+let openModalButton = document.querySelector('.main-action-button')
+openModalButton.addEventListener('click', openModal)
+
+// document.querySelector('.close-modal').addEventListener('click', () => {
+//     document.getElementById('modal').remove()
+// })
