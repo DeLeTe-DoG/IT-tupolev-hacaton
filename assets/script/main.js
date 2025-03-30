8e7e29b85e733fdfe34a193f6f9ae2d44b8aa12b
narr <vlad2006grigoriev@mail.ru>
2025-03-30 12:27:48 +0300

    чуть изменилоссь в футере, теперь выводится массив прямо из js

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,6 +5,23 @@ let rightBtn = document.querySelector('#right-footer-btn')
 currentItem = 1
 countOfItems = 5
 
+let item = `<div id="1" class="pages-of-gallery">
+            <img src="./assets/images/galleryFooter/footerImage.png" 
+            style="background-color: red;">
+            </img>
+            </div>`
+
+for (i = 1; i < countOfItems+1; i++) {
+    document.querySelector('#inner-screen-gallery').innerHTML += `
+            <div id="${i}" class="pages-of-gallery">
+            <img src="./assets/images/galleryFooter/footerImage${i}.png" 
+            style="background-color: red;">
+            </img>
+            </div>
+            `
+}
+
+
 leftBtn.addEventListener('click',() => {
     if (currentItem < countOfItems) {
         currentItem++
