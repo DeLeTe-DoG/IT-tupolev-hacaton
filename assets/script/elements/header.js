43fb682c0d37a6b4e504bd04218e5deefa8641a5
narr <vlad2006grigoriev@mail.ru>
2025-03-30 14:25:35 +0300

    Создана страница новостей "Крылья"

---
+++ assets/script/elements/header.js
@@ -0,0 +1,8 @@
+let header = `<img onclick="{ alert('huy') }" src="./assets/images/logo.png" alt="logo">
+            <div class="app-header__wrapper">
+                <h1 class="header-title">Казанский национальный исследовательский<br>технический университет</h1>
+                <h1 class="header-subtitle">Имени А.Н.Туполева</h1>
+            </div>`
+
+
+document.querySelector('.app-header').innerHTML += header
