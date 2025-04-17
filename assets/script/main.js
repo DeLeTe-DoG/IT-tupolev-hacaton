77ee9599830195844492be7c15485835295e25f0
narr <vlad2006grigoriev@mail.ru>
2025-04-17 22:17:50 +0300

    Сделан скролл на главной

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,24 @@
 // footer actions ----------------
 let leftBtn = document.querySelector('#right-footer-btn')
 let rightBtn = document.querySelector('#left-footer-btn')
-let screen = document.querySelector('#inner-screen-gallery')
+let footerScreen = document.querySelector('#inner-screen-gallery')
+// let screen = document.querySelector('.inner-main-image-section')
+const elem = document.querySelector('.main-pages-of-gallery')
+
+let frstMainPageSection = document.querySelector('#frstMainPageSection')
+let scndMainPageSection = document.querySelector('#scndMainPageSection')
 
 currentItem = 1
 countOfItems = 5
 
+
+// for main фоток
+currentMainItem = 1
+countOfMainItems = 5
+
+
 for (i = 1; i < countOfItems+1; i++) {
-        screen.innerHTML += `
+        footerScreen.innerHTML += `
             <div id="${i}" class="pages-of-gallery">
             <img src="./assets/images/galleryFooter/footerImage${i}.png" 
             style="background-color: red;">
@@ -15,6 +26,37 @@ for (i = 1; i < countOfItems+1; i++) {
             </div>
             `
 }
+
+
+
+for (i = 1; i < countOfMainItems+1; i++) {
+    frstMainPageSection.innerHTML += `
+        <div id="main${i}" class=" main-pages-of-gallery">
+        <img src="./assets/images/galleryFooter/footerImage${i}.png" 
+        style="background-color: red;">
+        </img>
+        </div>
+        `
+}
+for (i = 1; i < countOfMainItems+1; i++) {
+    scndMainPageSection.innerHTML += `
+        <div id="main${i}" class=" main-pages-of-gallery">
+        <img src="./assets/images/galleryFooter/footerImage${i}.png" 
+        style="background-color: red;">
+        </img>
+        </div>
+        `
+}
+
+function autoScroll() {
+    if (currentMainItem < countOfMainItems) {
+        currentItem++
+        elem.scrollIntoView({behavior: "smooth", block: "center", inline: "start"})
+    } else {
+        currentMainItem = currentMainItem
+    }
+}
+
 
 leftBtn.addEventListener('click',() => {
     if (currentItem < countOfItems) {
